8fb8a7d83c3a211c56d1437850610cd104df2313
PONSINTHU-01 <ponsindhu01@gmail.com>
2025-08-19 13:55:32 +0530

    hello

---
+++ 11-08.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello_World
+{
+    public class class1
+    {
+        static void Main(string[] args)
+        {
+            string name = "sinthu";
+            int num = 10;
+            int x = 10, y = 20, z = 30;
+            Console.WriteLine(x + y + z);
+            Console.WriteLine(name);
+            Console.Write(num);
+
+
+        }
+    }
+}
